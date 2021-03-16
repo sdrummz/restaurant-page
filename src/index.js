201eb04894d9dfcf31ecaf74ff4f4d4c921d9473
szglabs <sdrummond95@gmail.com>
2021-03-16 00:16:03 -0400

    home html layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello there");
+
+
+function createNewElement(type, classes, text) {
+    const element = document.createElement(type);
+    if (classes) {
+        for (let i = 0; i < classes.length; i++) {
+            element.classList.add(classes[i]);
+        }
+    }
+    if (text) {
+        element.innerHtml = text;
+    }
+}
+
+export { createNewElement }
